7d605cc544dc81c7f30205041618442a21cb5fea
Abdus Samee <abdus007samee@gmail.com>
2025-03-28 14:49:49 +0530

    comments get req added

--- 2/server.js
+++ 2/server.js
@@ -39,5 +39,19 @@ app.get('/test/users/:userid/posts', async (req,res)=>{
         console.log(error);
         res.sendStatus(500,`There was an error in : ${error}`)
     }
+})
 
+app.get('/test/posts/:postid/comments', async (req,res)=>{
+    const { postid } = req.params;
+    try {
+        const res = await axios.get("http://20.244.56.144/test/posts/:postid/comments", {headers: {
+            Authorization: `Bearer ${BEARER_TOKEN}`
+        }})
+        const users = res.data
+        res.status(200).json(users)
+        
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500,`There was an error in : ${error}`)
+    }
 })
